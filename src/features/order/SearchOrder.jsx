4b97eb4b8dfe13803dff99095e82a33d5fbd181a
DaTBagr <cal.yeates@hotmail.com>
2026-04-09 13:17:00 +0100

    Fixed header spacing

--- src/features/order/SearchOrder.jsx
+++ src/features/order/SearchOrder.jsx
@@ -19,7 +19,7 @@ function SearchOrder() {
         value={query}
         onChange={(e) => setQuery(e.target.value)}
         className="rounded-full px-4 py-2 text-sm bg-yellow-200
-          placeholder:text-stone-400 w-28 sm:w-48 sm:focus:w-72 transition-all
+          placeholder:text-stone-400 w-36 md:w-48 md:focus:w-72 transition-all
           duration-300 focus:outline-none focus:ring-yellow-500
           focus:ring-opacity-50"
       />
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -5,13 +5,20 @@ import Username from "../features/user/Username";
 function Header() {
   return (
     <header
-      className="flex items-center justify-between border-b border-stone-200
-        bg-yellow-400 px-4 py-3 uppercase sm:px-6"
+      className="relative flex items-center justify-between border-b
+        border-stone-200 bg-yellow-400 px-4 py-3 md:py-6 uppercase sm:px-6"
     >
       <Link to="/" className="tracking-widest">
         Big Pizza Co.
       </Link>
-      <SearchOrder />
+
+      <div
+        className="md:absolute md:left-1/2 md:top-1/2 md:-translate-x-1/2
+          md:-translate-y-1/2"
+      >
+        <SearchOrder />
+      </div>
+
       <Username />
     </header>
   );
